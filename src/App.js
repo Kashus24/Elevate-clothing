a8776734d055428f3bf4ea101587ee5b781cfb5d
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-03-08 21:47:55 -0500

    added outlet, link from react dom and imported logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+
+
+
+
 
 
 
@@ -8,8 +13,12 @@ const App = () => {
 
   return (
     <Routes>
+      <Route path="/" element={<Navigation/>}>
 
-      <Route path="/home" element={ <Home/>}/>
+      <Route index element={ <Home/>}/>
+
+
+      </Route>
 
     </Routes>
   );
