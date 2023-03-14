fe5877b536fb2675e2a94983f5b25170d81816c5
Akashdeep Sekhon <akashdeep24@hotmail.com>
2023-03-13 22:52:43 -0400

    setting handle change function to update values stores for sign up form

--- src/components/sign-up-form/sign-up-form-component.jsx
+++ src/components/sign-up-form/sign-up-form-component.jsx
@@ -12,8 +12,13 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword} = formFields;
 
-  const handleChange = (event) => {
 
+  console.log(formFields);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
   };
 
 
@@ -24,17 +29,41 @@ const SignUpForm = () => {
 
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" required onChange={handleChange}/>
+        <input 
+          type="text" 
+          required 
+          onChange={handleChange} 
+          name="displayName" 
+          value={displayName} 
+        />
 
         <label>Email</label>
-        <input type="email" required onChange={handleChange}/>
+        <input 
+          type="email" 
+          required 
+          onChange={handleChange} 
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input type="password" required onChange={handleChange}/>
+        <input 
+          type="password" 
+          required 
+          onChange={handleChange} 
+          name="password" 
+          value={password}
+        />
 
         <label>Confirm Password</label>
-        <input type="password" required onChange={handleChange}/>
-        
+        <input 
+          type="password" 
+          required 
+          onChange={handleChange} 
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
         <button type="submit">Sign Up</button>
 
       </form>
