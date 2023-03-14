3c7cc4c116c6e60131651f1f4a1dc8cff4a19a8c
Akashdeep Sekhon <akashdeep24@hotmail.com>
2023-03-13 22:09:39 -0400

    removed google re direct sign in/ staying simple with pop up

--- src/routes/sign-in/sign-in-component.jsx
+++ src/routes/sign-in/sign-in-component.jsx
@@ -1,16 +1,14 @@
 
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
 import { 
   auth,
   signInWithGooglePopup, 
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect
  } from "../../utils/firebase/firebase.utils";
+import { async } from "@firebase/util";
 
 
 const SignIn = () => {
+
 const logGoogleUser = async () => {
   const { user } = await signInWithGooglePopup();
   const userDocRef = await createUserDocumentFromAuth(user);
@@ -22,7 +20,6 @@ const logGoogleUser = async () => {
     <div>
       <h1> Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup </button>
-      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect </button>
     </div>
   );
 };
