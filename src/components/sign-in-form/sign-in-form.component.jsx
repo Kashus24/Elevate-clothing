6d88fb4b84009e667fc6664c81efbe59e4ebb7b4
Akashdeep Sekhon <akashdeep24@hotmail.com>
2023-03-17 17:28:34 -0400

    finished up major styling for sign in page, fixed minor google sign in issue

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -48,13 +48,25 @@ const SignInForm = () => {
 
     try {
       const response = await signInAuthUserWithEmailAndPassword(email, password);
+
       console.log(response);
-      
+
       resetFormFields();
 
-
     } catch(error) {
-    
+      switch (error.code) {
+
+        case "auth/wrong-password": 
+        alert('incorrect password for email');
+        break;
+
+        case "auth/user-not-found":
+          alert('no user associated with this email');
+          break;
+
+        default:
+          console.log(error);
+      }
     };
   };
 
@@ -96,7 +108,7 @@ const SignInForm = () => {
 
         <div className="buttons-container">
         <Button type="submit">Sign In</Button>
-        <Button buttonType="google"onClick={signInWithGoogle}>Google sign in</Button>
+        <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
 
         </div>
       </form>
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,21 +1,15 @@
 
-import { 
-  auth,
-  signInWithGooglePopup, 
-  createUserDocumentFromAuth,
- } from "../../utils/firebase/firebase.utils";
-import { async } from "@firebase/util";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form-component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-
+import "./authentication.styles.scss";
 
 const Authentication = () => {
 
   return (
-    <div>
-      <h1> Sign In Page</h1>
+    <div className="authentication-container">
+
       <SignInForm/>
       <SignUpForm/>
     </div>
