ab7a3c8edaf964504941dfd4441ca312d173129f
Akashdeep Sekhon <akashdeep24@hotmail.com>
2023-04-12 16:51:02 -0400

    added name and quantity to checkout page for items

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,33 @@
+
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import './checkout.styles.scss';
 
+
 const Checkout = () => {
+
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div>
       <h1>checkout page</h1>
+      <div>
+        {
+          cartItems.map((cartItem) => {
+
+            const {id, name, quantity} = cartItem;
+
+            return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+
+            </div>
+            );
+
+          })
+        }
+      </div>
     </div>
   )
 }
